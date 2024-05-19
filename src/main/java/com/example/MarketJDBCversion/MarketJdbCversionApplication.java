6cd66df41d4a75e73e1898b820723b3f1ff6a8d5
Daniel_Thevening <gonfidaniel@gmail.com>
2024-05-19 15:31:52 -0500

    first commmit

---
+++ src/main/java/com/example/MarketJDBCversion/MarketJdbCversionApplication.java
@@ -0,0 +1,13 @@
+package com.example.MarketJDBCversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketJdbCversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketJdbCversionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MarketJDBCversion/MarketJdbCversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MarketJDBCversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketJdbCversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
